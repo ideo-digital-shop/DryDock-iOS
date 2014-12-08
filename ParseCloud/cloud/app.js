d3ced5b72856ea6217c1458236254a887abf4074
Tim Shi <timshi@ideo.com>
2014-12-08 14:01:17 -0800

    removing VDDConstants from .gitignore

--- ParseCloud/cloud/app.js
+++ ParseCloud/cloud/app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var app = express();
 var versionTracker = require('cloud/versionTracker.js');
 
+var VDDModelApp = Parse.Object.extend("VDDModelApp");
+
 // Global app configuration section
 app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
@@ -21,6 +23,23 @@ app.get('/track/:channel', function(req, res) {
     return res.send(versionPackage);
   }, function(error) {
     return res.status(500).send(error);
+  });
+});
+
+app.get('/appdownload/:objectId', function(req, res) {
+  var modelAppQuery = new Parse.Query(VDDModelApp);
+  modelAppQuery.get(req.params.objectId).then(function(modelApp) {
+    if (!modelApp) {
+      
+    } else {
+      var renderPackage = {};
+      renderPackage['name'] = modelApp.get('name');
+      renderPackage['version_number'] = modelApp.get('version_number');
+      renderPackage['install_url'] = modelApp.get('install_url');
+      renderPackage['icon_url'] = modelApp.get('image').url();
+      renderPackage['description'] = modelApp.get('description');
+      return res.render('appdownload', renderPackage);
+    }
   });
 });
 
